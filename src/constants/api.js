3763b3b177f7be1b68f0fa1e885bf331c3fe4cd2
andrew <assdwqe@gmail.com>
2020-08-21 19:53:56 +0300

    upd category post page

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,11 +1,14 @@
+//define base url-s from requests to server
 export const BASE_API_URL = "https://topdevnews.pp.ua/wp-json";
 export const POSTS = "/wp/v2/posts";
 export const CATEGORIES = "/wp/v2/tags";
+//define different post type
 export const MAIN_POST_TYPE_ID = 5;
 export const TOP_POST_TYPE_ID = 4;
 export const RECOMEND_POST_TYPE_ID = 7;
 export const NOVELTY_POST_TYPE_ID = 8;
 export const STANDART_POST_TYPE_ID = 6;
+//every post type equal some string name, which will be used in route
 export const POST_TYPE = {
     [MAIN_POST_TYPE_ID]:'main',
     [TOP_POST_TYPE_ID]:'top',
--- src/page/CategoryPostPage.jsx
+++ src/page/CategoryPostPage.jsx
@@ -12,27 +12,35 @@ const CategoryPostPage = ({posts}) => {
     }
     return(
         <div className="home-wrap">
-             {posts.map((item,index) => (
-                <div className="col-4">
-                    <Link to={`/post/${POST_TYPE[item.categories[0]]}/${item.slug}`} className="base-post__link" key={index}>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <ul className="category-list m-0">
-                                {item.acf.post_categories && item.acf.post_categories.map(elem => (
-                                    <li className="category-list__item m-0 pr-3" key={elem.term_id}>{elem.name}</li>
-                                ))}
-                            </ul>
-                            <span className="main-post__external">{item.acf.post_external}</span>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-9">
+                        <div className="row">
+                            {posts.map((item,index) => (
+                                <div className="col-6">
+                                    <Link to={`/post/${POST_TYPE[item.categories[0]]}/${item.slug}`} className="base-post__link" key={index}>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <ul className="category-list m-0">
+                                                {item.acf.post_categories && item.acf.post_categories.map(elem => (
+                                                    <li className="category-list__item m-0 pr-3" key={elem.term_id}>{elem.name}</li>
+                                                ))}
+                                            </ul>
+                                            <span className="main-post__external">{item.acf.post_external}</span>
+                                        </div>
+                                        <div className="base-post__img-wrap category-post__img-wrap">
+                                            <img src={item.acf.post_image.sizes['twentyseventeen-featured-image']} alt={item.title.rendered} className="category-post__img filtered-img"/>
+                                        </div>
+                                        <div className="base-post__info d-flex justify-content-between mt-2">
+                                            <span className="base-post__date">{item.acf.post_date}</span>
+                                        </div>
+                                        <h2 className="base-post__caption mt-0 mb-2">{item.title.rendered}</h2>
+                                    </Link>
+                                </div>
+                            ))}
                         </div>
-                        <div className="base-post__img-wrap">
-                            <img src={item.acf.post_image.sizes['twentyseventeen-featured-image']} alt={item.title.rendered} className="base-post__img filtered-img"/>
-                        </div>
-                        <div className="base-post__info d-flex justify-content-between mt-2">
-                            <span className="base-post__date">{item.acf.post_date}</span>
-                        </div>
-                        <h2 className="base-post__caption mt-0 mb-2">{item.title.rendered}</h2>
-                    </Link>
+                    </div>
                 </div>
-            ))}
+            </div>
         </div>
     )
 }
